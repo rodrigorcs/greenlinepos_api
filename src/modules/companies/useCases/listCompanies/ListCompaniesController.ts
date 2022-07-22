96f62c3e795bbd1e771ad6e5a579e1f818a4e712
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2022-07-22 10:16:36 -0300

    Added relation between company and custumers

--- src/modules/companies/useCases/listCompanies/ListCompaniesController.ts
+++ src/modules/companies/useCases/listCompanies/ListCompaniesController.ts
@@ -3,8 +3,11 @@ import { ListCompaniesUseCase } from "./ListCompaniesUseCase";
 
 export class ListCompaniesController {
   async handle(request: Request, response: Response) {
+    const showCustomers = request.query.showCustomers === "true";
     const listCompaniesUseCase = new ListCompaniesUseCase();
-    const result = await listCompaniesUseCase.execute();
+    const result = await listCompaniesUseCase.execute({
+      showCustomers,
+    });
 
     return response.json(result);
   }
--- src/modules/companies/useCases/listCompanies/ListCompaniesUseCase.ts
+++ src/modules/companies/useCases/listCompanies/ListCompaniesUseCase.ts
@@ -1,8 +1,16 @@
 import { prisma } from "../../../../database/prismaClient";
 
+interface IListCompanies {
+  showCustomers: boolean;
+}
+
 export class ListCompaniesUseCase {
-  async execute() {
-    const companies = await prisma.companies.findMany();
+  async execute({ showCustomers }: IListCompanies) {
+    const companies = await prisma.companies.findMany({
+      include: {
+        customers: showCustomers,
+      },
+    });
 
     return companies;
   }
