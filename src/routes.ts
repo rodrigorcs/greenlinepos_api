9a83dff20449055da87953b5b502dbd042fd6828
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2022-07-22 09:14:42 -0300

    Added company routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
+import { CreateCompanyController } from "./modules/companies/useCases/createCompany/CreateCompanyController";
+import { DeleteCompanyController } from "./modules/companies/useCases/deleteCompany/DeleteCompanyController";
+import { ListCompaniesController } from "./modules/companies/useCases/listCompanies/ListCompaniesController";
+import { UpdateCompanyController } from "./modules/companies/useCases/updateCompany/UpdateCompanyController";
 
 const router = Router();
 
-router.get("/", (request, response) => {
-  return response.status(201).send();
-});
+const createCompanyController = new CreateCompanyController();
+const deleteCompanyController = new DeleteCompanyController();
+const listCompaniesController = new ListCompaniesController();
+const updateCompaniesController = new UpdateCompanyController();
+
+router.get("/companies/", listCompaniesController.handle);
+router.post("/companies/", createCompanyController.handle);
+router.put("/companies/:id", updateCompaniesController.handle);
+router.delete("/companies/:id", deleteCompanyController.handle);
 
 export { router };
